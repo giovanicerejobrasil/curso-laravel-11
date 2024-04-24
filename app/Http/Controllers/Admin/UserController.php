3baf572a4bc389fd0b1d76ab5b92250a61d20ee5
giovanicerejobrasil <giovanicerejobrasil@gmail.com>
2024-04-24 00:52:55 -0300

    :sparkles: feat(cadastro): criação do cadastro do usuário

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -15,4 +15,16 @@ class UserController extends Controller
 
         return view('admin.users.index', compact('users'));
     }
+
+    public function create()
+    {
+        return view('admin.users.create');
+    }
+
+    public function store(Request $request)
+    {
+        User::create($request->all());
+
+        return redirect()->route('users.index');
+    }
 }
---
+++ resources/views/admin/users/create.blade.php
@@ -0,0 +1,9 @@
+<h1>Novo Usuário</h1>
+
+<form action="{{ route('users.store') }}" method="post">
+  @csrf
+  <input type="text" name="name" id="name" placeholder="Nome">
+  <input type="email" name="email" id="email" placeholder="E-mail">
+  <input type="password" name="password" id="password" placeholder="Senha">
+  <input type="submit" value="Cadastrar">
+</form>
