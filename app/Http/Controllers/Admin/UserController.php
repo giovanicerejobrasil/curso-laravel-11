9a15975b61809eb3b72eea0cd6bce00a670c8917
giovanicerejobrasil <giovanicerejobrasil@gmail.com>
2024-04-26 00:28:14 -0300

    :sparkles: refactor(validation): refatoração da validação do update

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\StoreUserRequest;
+use App\Http\Requests\UpdateUserRequest;
 use App\Models\User;
 use Illuminate\Http\Request;
 
@@ -24,7 +25,7 @@ class UserController extends Controller
 
     public function store(StoreUserRequest $request)
     {
-        User::create($request->all());
+        User::create($request->validated());
 
         return redirect()
             ->route('users.index')
@@ -44,16 +45,22 @@ class UserController extends Controller
         return view('admin.users.edit', compact('user'));
     }
 
-    public function update(string $id, Request $request)
+    public function update(UpdateUserRequest $request, string $id)
     {
         if (!$user = User::find($id)) {
             return redirect()->back()->with('message', 'O usuário não foi encontrado');
         }
 
-        $user->update($request->only([
+        $data = $request->only([
             'name',
             'email'
-        ]));
+        ]);
+
+        if ($request->password) {
+            $data['password'] = bcrypt($request->password);
+        }
+
+        $user->update($data);
 
         return redirect()
             ->route('users.index')
--- resources/views/admin/users/edit.blade.php
+++ resources/views/admin/users/edit.blade.php
@@ -13,6 +13,6 @@
         <input type="text" name="name" id="name" placeholder="Nome" value="{{ $user->name }}">
         <input type="email" name="email" id="email" placeholder="E-mail" value="{{ $user->email }}">
         <input type="password" name="password" id="password" placeholder="Senha">
-        <input type="submit" value="Cadastrar">
+        <input type="submit" value="Editar">
     </form>
 @endsection
