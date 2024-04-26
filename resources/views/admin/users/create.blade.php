9a10559674984c3e56e576457085fbb1f96f1acc
giovanicerejobrasil <giovanicerejobrasil@gmail.com>
2024-04-26 00:39:29 -0300

    :reciclye: refactor(reutilização): reutilização de views para o formulário

--- resources/views/admin/users/create.blade.php
+++ resources/views/admin/users/create.blade.php
@@ -5,14 +5,8 @@
 @section('content')
     <h1>Novo Usuário</h1>
 
-    {{-- @include('admin.includes.errors') --}}
-    <x-alert />
-
     <form action="{{ route('users.store') }}" method="post">
         @csrf
-        <input type="text" name="name" id="name" placeholder="Nome" value="{{ old('name') }}">
-        <input type="email" name="email" id="email" placeholder="E-mail" value="{{ old('email') }}">
-        <input type="password" name="password" id="password" placeholder="Senha">
-        <input type="submit" value="Cadastrar">
+        @include('admin.users.partials.form')
     </form>
 @endsection
--- resources/views/admin/users/edit.blade.php
+++ resources/views/admin/users/edit.blade.php
@@ -5,14 +5,8 @@
 @section('content')
     <h1>Editar Usuário {{ $user->name }}</h1>
 
-    <x-alert />
-
     <form action="{{ route('users.update', $user->id) }}" method="post">
-        @csrf
         @method('put')
-        <input type="text" name="name" id="name" placeholder="Nome" value="{{ $user->name }}">
-        <input type="email" name="email" id="email" placeholder="E-mail" value="{{ $user->email }}">
-        <input type="password" name="password" id="password" placeholder="Senha">
-        <input type="submit" value="Editar">
+        @include('admin.users.partials.form')
     </form>
 @endsection
